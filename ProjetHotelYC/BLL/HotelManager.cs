5d620ceadf325e4ed69d29a8f1d9db95aa0c673e
Yann <yann.cla@gmail.com>
2018-12-09 18:00:44 +0100

    getAllHotels

--- ProjetHotelYC/BLL/HotelManager.cs
+++ ProjetHotelYC/BLL/HotelManager.cs
@@ -5,14 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using DTO;
 using DAL;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace BLL
 {
     public static class HotelManager
     {
+
         public static List<Hotel> GetAllHotels()
         {
-            return HotelDB.GetAllHotels();
+            List<Hotel> hotels;
+            string uri = "http://localhost:3749/api/Hotels/";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<String> response = httpClient.GetStringAsync(uri);
+                hotels = JsonConvert.DeserializeObject<List<Hotel>>(response.Result);
+            }
+            return hotels;
         }
 
         public static Hotel GetHotel(int IdHotel)
